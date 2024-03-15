816fe8127d31f27bf501301a2b39b107aa5220f4
big-brother-sudo <ballaistvangabor@gmail.com>
2024-03-15 15:06:37 +0100

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/user/user.component.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,46 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-@Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
-})
-export class UserComponent implements OnInit {
-
-//2b. feladat
-// a userData osztályváltozó típusának beállítsa User típusúra
-  userData: User;
-
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient
-  ) { }
-
-  id: string = "-1";
-
-  ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id')!
-    //alert(this.id)
-    this.getUser(this.id)
-  }
-//2a. feladat 
-// GET kérés típusának beállítása User típusúra:
-  getUser(id: string){
-    let url = "https://jsonplaceholder.typicode.com/users/" + id;
-    this.http.get<User>(url).subscribe(
-      res=>{
-        this.userData = res;
-      }
-    )
-  }
-
-}
-
-//1. feladat
-// Minden tulajdonság definiálása az User típusban a https://jsonplaceholder.typicode.com/users/1
+// 1. fel.: Minden tulajdonság definiálása az User típusban a 
+// https://jsonplaceholder.typicode.com/users/1
 // responsa alapján:
-type User = {
+export type User = {
   id: number,
   name: string,
   username: string,
@@ -63,4 +27,38 @@ type User = {
     catchPhrase: string,
     bs: string
   }
+}
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  // 2a. fel.: Az userData osztályváltozó típusának beállítsa User típusúra
+  userData: User = {} as User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
+
+  id: string = "-1";
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id')!
+    //alert(this.id)
+    this.getUser(this.id)
+  }
+  // 2b. fel.:GET kérés típusának beállítása User típusúra:
+  getUser(id: string){
+    let url = "https://jsonplaceholder.typicode.com/users/" + id;
+    this.http.get<User>(url).subscribe(
+      res=>{
+        this.userData = res;
+      }
+    )
+  }
+
 }
